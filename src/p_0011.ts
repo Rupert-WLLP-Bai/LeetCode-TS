a67654c8a3da809d087b10a0c67774dcdc578e60
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-12-21 08:57:53 +0000

    p11 ~ p12

---
+++ src/p_0011.ts
@@ -0,0 +1,32 @@
+// LeetCode - 11
+
+/**
+ * 盛最多水的容器
+ * @param height 高度数组
+ * @returns 最大容量
+ */
+export function maxArea(height: number[]): number {
+    // 双指针法
+    let left = 0
+    let right = height.length - 1
+
+    // 记录最大容量
+    let max = 0
+
+    while (left < right) {
+        // 计算当前容量
+        const current = (right - left) * Math.min(height[left], height[right])
+        // 更新最大容量
+        max = Math.max(max, current)
+
+        // 移动指针, 如果左边的高度小于右边的高度，则左指针右移，否则右指针左移
+        // 因为如果左边的高度小于右边的高度，那么无论右指针怎么移动，容量都不会超过当前容量
+        if (height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+
+    return max
+}
---
+++ tests/p_0011.test.ts
@@ -0,0 +1,32 @@
+import { maxArea } from '../src/p_0011'
+
+const testCases = [
+    {
+        input: [1, 8, 6, 2, 5, 4, 8, 3, 7],
+        answer: 49
+    },
+    {
+        input: [1, 1],
+        answer: 1
+    },
+    {
+        input: [4, 3, 2, 1, 4],
+        answer: 16
+    },
+    {
+        input: [1, 2, 1],
+        answer: 2
+    },
+    {
+        input: [1, 2, 4, 3],
+        answer: 4
+    }
+]
+
+describe('LeetCode: 11 盛最多水的容器', () => {
+    testCases.forEach(testCase => {
+        it(`测试用例 [${testCase.input}]`, () => {
+            expect(maxArea(testCase.input)).toEqual(testCase.answer)
+        })
+    })
+})
